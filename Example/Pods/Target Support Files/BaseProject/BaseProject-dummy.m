2b8b58d5ef1764ea445c4bcef274656bd869f063
wa  ° <oncwnuOFQIPUqFMr48EisEyZQvmM@git.weixin.qq.com>
2020-04-02 11:03:54 +0800

    xx

---
+++ Example/Pods/Target Support Files/BaseProject/BaseProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BaseProject : NSObject
+@end
+@implementation PodsDummy_BaseProject
+@end
---
+++ Example/Pods/Target Support Files/Pods-BaseProject_Example/Pods-BaseProject_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BaseProject_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BaseProject_Example
+@end
